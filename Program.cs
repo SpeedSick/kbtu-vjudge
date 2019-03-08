8a9f1dffb035fddc13dcd16ee7abe642cb16534a
alan <alan@relog.kz>
2019-03-08 23:41:25 +0600

    hello world app

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace kbtu_vjudge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
